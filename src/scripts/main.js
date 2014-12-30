4b0433de273c80a89b05d8afa7b971a095b3f65e
mavidser <mavidser@gmail.com>
2014-12-30 18:25:10 +0530

    top part done, i think.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,7 +9,7 @@ function sameHeight(a) {
 
 $(document).ready(function () {
 
-  $(".cover-size").height($(window).height());
+  $("#top").height($(window).height());
 
   $("a[href*=#]:not([href=#])").click(function () {
     if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") && location.hostname == this.hostname) {
@@ -28,25 +28,4 @@ $('.thumb-image').load(function() {
 $(window).resize(function () {
   $(".cover-size").height($(window).height());
   sameHeight($(".thumbnail-container > .thumbnail"));
-});
-
-// var elementPosition = $('.navbar').offset();
-// var elementPosition = $('#lower').offset().top+$('.navbar').position().top;
-var elementPosition = 0;
-$(window).scroll(function(){
-  // console.log('checking',$('.navbar').css('position'));
-  // var elementPosition = $('.navbar').position().top+$('.navbar').offset().top;
-  if ($('.navbar').css('position') == 'relative')
-    elementPosition = $('.navbar').offset().top;
-  // var elementPosition = $('#lower').offset().top+$('.navbar').position().top;
-  // console.log($(window).scrollTop(),$('.navbar').position().top,$('.navbar').offset().top,elementPosition);
-  if($(window).scrollTop() >= elementPosition){
-    // $('.navbar').css('position','fixed').css('top','0');
-    $('.navbar').addClass('fixed-top');
-    // $('.blank').css('height','50px');
-  } else {
-    $('.navbar').removeClass('fixed-top');
-    // $('.navbar').css('position','relative');
-    // $('.blank').css('height','0px');
-  }
 });
